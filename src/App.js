4717568672d18b864409eeb52602841f712180e5
apple10001 <line@lightgreen.main.jp>
2020-07-16 12:18:15 +0900

    create template before adding bootstrap library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React , { useState, useEffect } from 'react'
 
-const App = () => {
+const App = props => {
+  const [state, setState] = useState(props)
+  const {name, price} = state
+
+  useEffect(() => {
+    console.log('didMount and didUpdate')
+  })
+
+  useEffect(() => {
+    console.log('useEffect!MountOnly ')
+  },[])
+
+  useEffect(() => {
+    console.log('THis callback 9s name only')
+  },[name])
+  
   return (
-    <div>
-      <p>hello</p>
-    </div>
+    <>
+      <p>現在の{name}は、{price}です</p>
+      <button onClick={() => setState({...state, price: price+1})}>+1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={e =>setState({...state, name: e.target.value})}/>
+    </>
   )
+}
+
+App.defaultProps = {
+  name: 'a',
+  price: 1000
 }
 
 export default App;
